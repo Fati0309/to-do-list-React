9cda7ab10915eb11622ac6b8d26fa169f0ddedb4
lazreug <lazreug.fati@gmail.com>
2021-09-19 14:46:53 +0100

    rest clear de input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import React, { useState } from "react";
+export default function App(props) {
+  const [list, setlist] = useState([]);
+  const [value, setvalue] = useState("");
+  const Item = ({ item }) => {
+    return (
+      <div className="items">
+        <p>{item}</p>
+        <button onClick={supvalue}>--</button>
+      </div>
+    );
+  };
+  const handleChange = (e) => {
+    let x;
+    x = setvalue(e.target.value);
+    return x;
+  };
+  const supvalue = () => {
+    let x = list;
+    x.splice(x.indexOf(value), 1);
+    setlist([...x]);
+    console.log(list);
+  };
+  const handleSubmit = (e) => {
+    if (value === "") {
+      alert("ecrire votre etape");
+      console.log("vide");
+    } else {
+      let x = list;
+      x.push(value);
+      setlist([...x]);
+      console.log(list);
+    }
+  };
+  const handlesupp = (e) => {
+    setlist([]);
+    console.log(list);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenu">
+        <h1> To Do Liste</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="edit votre etape "
+            minLength="2"
+            onChange={handleChange}
+          ></input>
+          <button onClick={handleSubmit}> add</button>
+          <button onClick={handlesupp}>clear</button>
+        </div>
+        {list.map((value) => {
+          return <Item item={value} />;
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
